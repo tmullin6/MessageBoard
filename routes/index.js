3aff75644f3ae34304fc260d7f6d723b3df304da
tmullin6 <tyleramullins@gmail.com>
2021-10-17 00:34:02 -0600

    Stylesheet Update

--- routes/index.js
+++ routes/index.js
@@ -5,30 +5,35 @@ let messages = [
   {
     text:"I love this website",
     name:"Tyler",
-    date: new Date()
+    date: new Date().toDateString()
   },
   {
     text:"Hello World!",
     name:"Estelle",
-    date: new Date()
+    date: new Date().toDateString()
   },
   {
     text:"I can't wait to see what else this site can do",
     name:"Taylor",
-    date: new Date()
+    date: new Date().toDateString()
   }
 ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Speak Up', pageTitle: 'Speak Your Mind', messages: messages });
+  res.render('index', 
+  { 
+    title: 'Speak Up', 
+    pageTitle: 'Speak Your Mind', 
+    messages: messages 
+  });
 });
 
 router.post("/new",(req,res)=>{
   let newMessage={
     name: req.body.name,
     text: req.body.text,
-    date: new Date()
+    date: new Date().toDateString()
   };
 
   messages.push(newMessage);
